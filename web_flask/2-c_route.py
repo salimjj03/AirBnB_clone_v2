fd31ce745a1c3d049f83fd695d08c12fe6f45ea7
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-02-18 13:34:51 -0800

    flask frame

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,5 +21,18 @@ def hbnb():
     return "HBNB"
 
 
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ This used variable to dispay text """
+    txt = ""
+    if "_" in text:
+        for i in range(len(text)):
+            if text[i] == "_":
+                txt = txt+" "
+            else:
+                txt = txt+text[i]
+    return "c {}".format(txt)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
